4d3e30c7f977142108fe84d3e646fa5ab6071c11
aurelita66 <aurelija.aura0312@gmail.com>
2024-11-29 14:31:10 +0200

    Sukuriamas vartotojo meniu

---
+++ minibiudžetas.py
@@ -0,0 +1,7 @@
+while True:
+    print('1. Ivesti pajamas\n'
+          '2. Ivesti islaidas\n'
+          '3. Atspausdinti pajamu eilutes\n'
+          '4. Atspausdinti islaidu eilutes\n'
+          '5. Atspausdinti statistika\n'
+          'q - Iseiti\n')
